2f82baadda3bbc5f52a93df0a5fc652b1d341116
Chitresh Gyanani <129378666+Chitresh-code@users.noreply.github.com>
2024-08-09 22:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 from PIL import Image
 import google.generativeai as genai
-import fitz  # PyMuPDF
+import fitz
 from io import BytesIO
 
 # Load environment variables
